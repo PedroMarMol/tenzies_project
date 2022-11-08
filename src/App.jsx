bfb2b9d2ef94f3c83b3ae99460363e0c20a942be
PedroMarMol <martosmoleropedro@gmail.com>
2022-11-08 10:49:07 +0100

    map array to die components

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,11 @@ import React, { useState } from 'react';
 import "./App.css"
 import Die from "./components/Die"
 
-/**
- * Challenge:
- * 
- * Write a function (allNewDice) that returns an array 
- * of 10 random numbers between 1-6 inclusive.
- * 
- * Log the array of numbers to the console for now
- */
-
 export default function App() {
     const [isActive, setIsActive] = useState(false)
-    const handleClick = () => {
-      setIsActive(current => !current) 
-    } 
+    const handleClick = (() => setIsActive(current => !current))
+
+    const [arrayOfNumbers, setArrayOfNumbers] = useState(allNewDice)
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
@@ -23,6 +14,9 @@ export default function App() {
         }
         return newDice
     }
+    
+    const diceNumbers = arrayOfNumbers.map((die) => <Die value={die} />)
+    
     return (
         <main className="main--app">
             <p className="main--header">
@@ -32,16 +26,7 @@ export default function App() {
                 Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
             </p>
             <div className='dice--container'>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
-                <Die value="1"/>
+                {diceNumbers}
             </div>
             <button className="main--button">Roll</button>
         </main>

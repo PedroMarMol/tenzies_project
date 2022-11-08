ad47acf803ffb184bdca2d0a4f653660fa9e2cf4
PedroMarMol <martosmoleropedro@gmail.com>
2022-11-08 09:58:36 +0100

    create die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import "./App.css"
+import Die from "./components/Die"
 
 export default function App() {
     const [isActive, setIsActive] = useState(false)
@@ -14,68 +15,18 @@ export default function App() {
             <p className="main--text">
                 Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
             </p>
-            <div className="main--grid">
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                <button 
-                    className="main--numbers" 
-                    style={{backgroundColor: isActive ? '#59E391' : 'white'}}
-                    onClick={handleClick}
-                >   1
-                </button>
-                </div>
+            <div className='dice--container'>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+                <Die value="1"/>
+            </div>
             <button className="main--button">Roll</button>
         </main>
     )
---
+++ src/components/Die.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Die(props) {
+    const [isActive, setIsActive] = React.useState(false)
+    function handleClick() {
+        setIsActive(prev => !prev)
+    }
+    return (
+        <div>
+            <button 
+                className="main--numbers"
+                style={{backgroundColor: isActive ? '#59E391' : 'white'}}
+                onClick={handleClick}
+                >{props.value}
+            </button>
+        </div>
+    )
+}
